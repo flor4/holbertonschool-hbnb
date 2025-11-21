5cefd6a33936017b51f5474ff36cf02187d6c572
flor4 <salanson.flora@outlook.fr>
2025-11-21 21:26:56 +0100

    update: init to manages the front part of the project

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -2,13 +2,20 @@ from flask import Flask
 from flask_restx import Api
 from app.Extensions import jwt, bcrypt, db
 from config import DevelopmentConfig
+from flask_cors import CORS
 
+# Import des namespaces
 from app.api.v1.places import api as places_ns
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.auth import api as auth_ns
 
+# Import explicite des modèles pour que SQLAlchemy connaisse toutes les tables
+from app.models.user import User
+from app.models.place import Place
+from app.models.amenity import Amenity
+from app.models.review import Review
 
 authorizations = {
     'Bearer': {
@@ -23,16 +30,15 @@ def create_app(config_class=DevelopmentConfig):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize SQLAlchemy with app
+    CORS(app, resources={r"/api/*": {"origins": "http://localhost:8000"}}, supports_credentials=True)
+
+
+    # Initialisation des extensions
     db.init_app(app)
-
-    # Set up bcrypt
     bcrypt.init_app(app)
-
-    # Set up jwt
     jwt.init_app(app)
 
-    # Set up API
+    # Initialisation de l'API
     api = Api(
         app,
         version='1.0',
@@ -42,16 +48,15 @@ def create_app(config_class=DevelopmentConfig):
         authorizations=authorizations,
     )
 
-    # Register namespaces
+    # Enregistrement des namespaces
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(auth_ns, path='/api/v1/auth')
 
-
+    # Création des tables
     with app.app_context():
-        from app.models.amenity import Amenity
-        db.create_all()
+        db.create_all()  # toutes les tables importées sont créées ici
 
     return app
